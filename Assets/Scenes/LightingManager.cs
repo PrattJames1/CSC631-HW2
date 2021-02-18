799e339b2f233bc5b35ed1305fd32a9e8c43bb08
James Pratt <justafluke.io@gmail.com>
2021-02-18 08:00:59 -0800

    Adjusted directional light

--- Assets/Scenes/LightingManager.cs
+++ Assets/Scenes/LightingManager.cs
@@ -1,28 +1,26 @@
 using UnityEngine;
 
 [ExecuteAlways]
-
 public class LightingManager : MonoBehaviour
 {
-    //References
+    //Scene References
     [SerializeField] private Light DirectionalLight;
     [SerializeField] private LightingPreset Preset;
     //Variables
     [SerializeField, Range(0, 24)] private float TimeOfDay;
 
+
     private void Update()
     {
         if (Preset == null)
             return;
+
         if (Application.isPlaying)
         {
-            //TimeOfDay = 12; //High noon
-            
-            TimeOfDay += 0.01f; //Night changes to day
-            //TimeOfDay += TimeOfDay.deltaTime;
-            TimeOfDay %= 24; //Clamp between 0-24
+            //(Replace with a reference to the game time)
+            TimeOfDay += Time.deltaTime;
+            TimeOfDay %= 24; //Modulus to ensure always between 0-24
             UpdateLighting(TimeOfDay / 24f);
-            
         }
         else
         {
@@ -30,28 +28,37 @@ public class LightingManager : MonoBehaviour
         }
     }
 
+
     private void UpdateLighting(float timePercent)
     {
+        //Set ambient and fog
         RenderSettings.ambientLight = Preset.AmbientColor.Evaluate(timePercent);
         RenderSettings.fogColor = Preset.FogColor.Evaluate(timePercent);
 
+        //If the directional light is set then rotate and set it's color, I actually rarely use the rotation because it casts tall shadows unless you clamp the value
         if (DirectionalLight != null)
         {
-            //DirectionalLight.color = Color.white;
-
             DirectionalLight.color = Preset.DirectionalColor.Evaluate(timePercent);
-            DirectionalLight.transform.localRotation = Quaternion.Euler(new Vector3((timePercent * 360f) - 90f, -170, 0));
+
+            DirectionalLight.transform.localRotation = Quaternion.Euler(new Vector3((timePercent * 360f) - 90f, 170f, 0));
         }
+
     }
 
+
+
+    //Try to find a directional light to use if we haven't set one
     private void OnValidate()
     {
         if (DirectionalLight != null)
             return;
+
+        //Search for lighting tab sun
         if (RenderSettings.sun != null)
         {
             DirectionalLight = RenderSettings.sun;
         }
+        //Search scene for light that fits criteria (directional)
         else
         {
             Light[] lights = GameObject.FindObjectsOfType<Light>();
